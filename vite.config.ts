2fd9026d037ef3557d2a3cfff6484d41e9cf2ba0
Sejal-amreliya <sejalamreliya1996@gmail.com>
2026-09-25 11:26:24 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,16 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, './src'),
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom', 'react-router-dom'],
+            charts: ['recharts'],
+          },
+        },
+      },
+    },
     server: {
       proxy: {
         // Patient-service :8101
